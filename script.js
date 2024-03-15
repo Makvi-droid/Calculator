211b24210d05d3b329a021cab816c95ecb7484d9
Makvi-droid <pasbenauro05@gmail.com>
2024-03-15 14:44:51 +0800

    added buttons

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const subtract = document.querySelector('#subtract');
 const divide = document.querySelector('#divide');
 const multiply = document.querySelector('#multiply');
 const equals = document.querySelector('#equals');
+const input = document.querySelector('input');
+const span = document.querySelector('.span');
+const point = document.querySelector('#point');
+const remainder = document.querySelector('#remainder');
+const allClear = document.querySelector('#clear-all');
 const num1 = document.querySelector('#num1');
 const num2 = document.querySelector('#num2');
 const num3 = document.querySelector('#num3');
@@ -14,3 +19,47 @@ const num7 = document.querySelector('#num7');
 const num8 = document.querySelector('#num8');
 const num9 = document.querySelector('#num9');
 const num0 = document.querySelector('#num0');
+
+clear.addEventListener('click', function(){
+    input.value = "";
+});
+
+num1.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num2.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num3.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num4.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num5.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num6.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num7.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num8.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num9.addEventListener('click', function(){
+    input.value = this.textContent;
+});
+
+num0.addEventListener('click', function(){
+    input.value = this.textContent;
+});
